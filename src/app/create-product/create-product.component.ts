110542c6b7746f83ec616fcdd7ed42554427ec44
Sharon Florez <shdflorezs35@gmail.com>
2023-10-29 03:37:18 -0500

    Remove unnecessary code

--- src/app/create-product/create-product.component.ts
+++ src/app/create-product/create-product.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { nanoid } from 'nanoid';
 import Swal from 'sweetalert2';
 
 import { ProductsService } from 'src/services/products.service';
